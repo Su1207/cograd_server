1b99e540ac74098c20c49ac6691fd0bf0dbf0734
suraj maheshwari <surajmaheshwari159@gmail.com>
2024-03-23 22:05:23 +0530

    next commit

--- models/addressModel.js
+++ models/addressModel.js
@@ -4,16 +4,16 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const AddressSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String },
-    contact: { type: String },
-    contactCountry: { type: String },
-    billingAddress: { type: String },
-    landmark: { type: String },
-    pincode: { type: String },
-    district: { type: String },
-    state: { type: String },
+  userId: { type: String },
+  name: { type: String },
+  email: { type: String },
+  contact: { type: String },
+  contactCountry: { type: String },
+  billingAddress: { type: String },
+  landmark: { type: String },
+  pincode: { type: String },
+  district: { type: String },
+  state: { type: String },
 });
 
 module.exports = mongoose.model("Address", AddressSchema);
-
