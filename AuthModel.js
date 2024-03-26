58988101de35f96add07e27fb0fcf0c40bbd9604
Suraj Maheshwari <surajmaheshwari159@gmail.com>
2024-03-27 02:41:48 +0530

    Add files via upload

---
+++ AuthModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is Required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
+    },
+    role: { type: String, required: true, default: "user" },
+    userName: { type: String, required: [true, "Username is Required"] },
+    phoneNumber: { type: String, required: [true, "Phone Number is Required"] },
+    gender: String,
+    // dob: { type: Date, required: [true, "Date of Birth is Required"] },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("incorrect password");
+  }
+  throw Error("incorrect email");
+};
+
+exports.Users = mongoose.model("Users", userSchema);
