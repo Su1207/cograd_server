5e7c927602fc7b8157444e49942c87b287b8f799
suraj maheshwari <surajmaheshwari159@gmail.com>
2024-03-27 01:05:06 +0530

    next commit order edited

--- Controllers/AuthControllers.js
+++ Controllers/AuthControllers.js
@@ -1,4 +1,4 @@
-const User = require("../models/authModel");
+const User = require("../models/AuthModel");
 const jwt = require("jsonwebtoken");
 
 const maxAge = 3 * 24 * 60 * 60;
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,7 +1,9 @@
-const { register, login, address } = require("../controllers/authControllers");
-const { checkUser } = require("../middlewares/authMiddleware");
-const { storeAddress, findAddressesByUserId } = require("../controllers/addressController.js")
-
+const { register, login, address } = require("../controllers/AuthControllers");
+const { checkUser } = require("../middlewares/AuthMiddleware.js");
+const {
+  storeAddress,
+  findAddressesByUserId,
+} = require("../controllers/AddressController.js");
 
 const router = require("express").Router();
 //secret page, checkuser
@@ -9,8 +11,6 @@ router.post("/cards", checkUser);
 router.post("/signup", register);
 router.post("/login", login);
 router.post("/address", storeAddress); // New endpoint for storing address
-router.get("/getaddress/:userId", findAddressesByUserId)
-
-
+router.get("/getaddress/:userId", findAddressesByUserId);
 
 module.exports = router;
